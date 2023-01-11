d00c2ab26c548c9800c5935ff67773595d4d5aa1
EVANDERSON <axel_steel@live.com>
2023-01-11 17:45:52 -0300

    Concluido o codigo faltante, e refatorado.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,38 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val id: Int, val nome: String, val idade: Int, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    var inscritos = mutableListOf<Usuario>()
+
+    fun matricular(usuarios: List<Usuario>) {
+        for (usuario in usuarios) {
+            if (inscritos.contains(usuario)) {
+            	println("Usuário ${usuario.nome} já está matriculado nesta formação!")
+        	} else {
+            	inscritos.add(usuario)
+            	println("Usuário ${usuario.nome} matriculado com sucesso!")
+        	}
+        }        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario(1,"Evanderson Ribeiro", 35, "evanderson@email.com")
+    val usuario2 = Usuario(2,"Dr. House", 45, "drhouse@email.com")
+    
+    val conteudos = listOf(ConteudoEducacional("Introdução Prática à Linguagem de Programação Kotlin"), //POR PADRÃO SERÁ UM CONTEÚDO DE 60 MINUTOS
+                           ConteudoEducacional("Orientação a Objetos e Tipos de Classes na Prática com Kotlin", 120),
+                           ConteudoEducacional("Abstraindo Formações da DIO Usando Orientações a Objetos com Kotlin", 120))
+    
+    val formacao = Formacao("Kotlin Experience", conteudos, Nivel.BASICO)
+    println(formacao)
+    
+    println()
+    
+    val usuarios = listOf(usuario1, usuario2)
+    formacao.matricular(usuarios)
 }
